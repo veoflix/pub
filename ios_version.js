53ae939d621cceb5e1ff44eca8bd5f38691d314e
veoflix <robrayo23@gmail.com>
2026-08-15 23:13:37 +0800

    Create ios_version.js

---
+++ ios_version.js
@@ -0,0 +1,8 @@
+{
+  "min_version":"1.2.0",
+  "latest_version":"1.2.0",
+  "force":"true",
+  "store_url":"https://apps.apple.com/app/[id]",
+  "title":"Update Required",
+  "message":"Please update Short Drama to keep watching."
+}
